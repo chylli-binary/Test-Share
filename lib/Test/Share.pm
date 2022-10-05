346b86df054afc9d319a6637ad1b9f8beb6237df
chylli-binary <chylli@binary.com>
2022-10-05 03:10:31 +0000

    initial commit

---
+++ lib/Test/Share.pm
@@ -0,0 +1,36 @@
+package Test::Share;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Test::Share - Module abstract
+
+=head1 SYNOPSIS
+
+    use Test::Share;
+    my $instance = Test::Share->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
